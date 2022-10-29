fb38b299d0a1973e5287b59301c8f782fb66fcb3
Ruslan <volopin@gmail.com>
2022-10-29 10:37:00 +0300

    123

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
+from Wiki import search_wiki
 
 TOKEN = "5538204452:AAFX8CEIaueT3XY_edY4d6I91hLRib1JvYY"
 
@@ -14,6 +15,16 @@ def echo(update, context):
 def start(update, context):
     update.message.reply_text("Бот запущен!")
 
+def wiki(update, context):
+        print(context.args)
+        word = "".join(context.args)
+        if word:
+            update.message.reply_text("Идет поиск...")
+            summary, url = search_wiki(word)
+            update.message.reply_text(summary+url)
+        else:
+            update.message.reply_text("Необходимо ввести что искать")
+
 
 def main():
     updater = Updater(TOKEN, use_context = True)
@@ -21,6 +32,7 @@ def main():
     print("Бот запущен")
 
     dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('wiki', wiki))
     dp.add_handler(MessageHandler(Filters.text, echo))
 
     updater.start_polling()
